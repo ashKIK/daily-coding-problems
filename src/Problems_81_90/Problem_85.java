8f5eb78ee08f5aa6533ea5c2d39386a015236d3e
ashKIK <ashutheprogrammer@gmail.com>
2019-07-01 21:24:22 +0530

    problem-80-90-added

---
+++ src/Problems_81_90/Problem_85.java
@@ -0,0 +1,27 @@
+package Problems_81_90;
+
+// Facebook.
+//
+// Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0,
+// using only mathematical or bit operations. You can assume b can only be 1 or 0.
+
+/**
+ * @author ashKIK
+ */
+public class Problem_85 {
+
+  private static int iAbs(int v) {
+    int mask = v >> 31;
+    return (v ^ mask) - mask;
+  }
+
+  private static int getNum(int x, int y, int b) {
+    return x * b + y * iAbs((b - 1));
+  }
+
+  public static void main(String... args) {
+
+    System.out.println(getNum(5, 8, 0));
+    System.out.println(getNum(5, 8, 1));
+  }
+}
---
+++ src/Problems_81_90/Problem_90.java
@@ -0,0 +1,51 @@
+package Problems_81_90;
+
+// Google.
+//
+// Given an integer n and a list of integers l,
+// write a function that randomly generates a number from 0 to n-1 that isn't in l (uniform).
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author ashKIK
+ */
+public class Problem_90 {
+
+  Random random;
+  int listSize = 0;
+  List<int[]> list;
+
+  private void build(int N, int[] blacklist) {
+    random = new Random();
+
+    Arrays.sort(blacklist);
+
+    list = new ArrayList<>();
+
+    int start = 0;
+    for (int value : blacklist) {
+      if (start != value) {
+        list.add(new int[]{start, value});
+        start = value + 1;
+      } else {
+        start++;
+      }
+    }
+
+    if (start < N) {
+      list.add(new int[]{start, N});
+    }
+
+    listSize = list.size();
+  }
+
+  public int pick() {
+    int index = random.nextInt(listSize);
+    int[] next = list.get(index);
+    return random.nextInt(next[1] - next[0]) + next[0];
+  }
+}
